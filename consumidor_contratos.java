870190cc757e0848ee746a0e334b5a0acabec5dc
CAUE GONCALVES <caue.goncalves@cs.up.edu.br>
2025-08-15 20:23:30 -0300

    Continuação 2

--- consumidor_contratos.java
+++ consumidor_contratos.java
@@ -3,9 +3,10 @@ public class consumidor_contratos {
         consumidor operador = new consumidor();
         contrato_compra_venda c = new contrato_compra_venda();
         contrato_prestacao_servico s = new contrato_prestacao_servico();
+        contrato_locacao l = new contrato_locacao();
 
         //Consumir o contrato de compra e venda
         //c.mercadoria = "Alcool";
-        operador.processar(s);
+        operador.processar(l);
     }
 }
--- contrato_locacao.java
+++ contrato_locacao.java
@@ -1,3 +1,11 @@
-public class contrato_locacao {
+public class contrato_locacao extends contrato {
     public float valor;
+
+    @Override
+    public boolean Salvar(){
+        if(this.situacao.equals("ativo")){
+            return true;
+        }
+        return false;
+    }
 }
